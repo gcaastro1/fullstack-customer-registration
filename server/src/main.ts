23bebbc007df5bbfd98cfeb0dbbd907c646573d2
gustavo <gcaastro1@gmail.com>
2023-06-04 21:27:37 -0300

    feat: backend doc

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './database/prisma.service';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('MyConts')
+    .setDescription('Uma agenda de contatos feita especialmente para você!')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true }),
